c1aac1a663579e3f2af92472cfac483857bf7152
Imran Ahmed <96imranahmed@gmail.com>
2017-09-10 03:48:59 +0100

    Added favicon, particle.js

--- application.py
+++ application.py
@@ -1,35 +1,19 @@
-from flask import Flask
+from datetime import datetime
 import pandas as pd
+from flask import Flask, abort, flash, redirect, render_template, request, url_for
 
-# print a nice greeting.
-def say_hello(username = "World"):
-    return '<p>Hello %s!</p>\n' % username
 
-# some bits of text for the page.
-header_text = '''
-    <html>\n<head> <title>Scholarly</title> </head>\n<body>'''
-instructions = '''
-    <p><em>Hint</em>: This is up and running! RESTful web service! Append a username
-    to the URL (for example: <code>/Thelonious</code>) to say hello to
-    someone specific.</p>\n'''
-home_link = '<p><a href="/">Back</a></p>\n'
-footer_text = '</body>\n</html>'
+app = Flask(__name__)
 
-# EB looks for an 'application' callable by default.
-application = Flask(__name__)
+@app.route('/')
+def main():
+    return render_template('index.html', title='Landing!')
 
-# add a rule for the index page.
-application.add_url_rule('/', 'index', (lambda: header_text +
-    say_hello() + instructions + footer_text))
-
-# add a rule when the page is accessed with a name appended to the site
-# URL.
-application.add_url_rule('/<username>', 'hello', (lambda username:
-    header_text + say_hello(username) + home_link + footer_text))
+@app.route('/<name>')
+def hello_name(name):
+    return "Hello {}!".format(name)
 
 # run the app.
 if __name__ == "__main__":
-    # Setting debug to True enables debug output. This line should be
-    # removed before deploying a production app.
-    application.debug = True
-    application.run()
+    app.debug = True
+    app.run()
---
+++ static/js/index.js
@@ -0,0 +1,3 @@
+particlesJS.load('particles-js', '/static/js/particle_config.json', function() {
+    console.log('callback - particles.js config loaded');
+});
