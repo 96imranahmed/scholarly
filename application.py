b69e3c15c13a22632fc9534d9534b56b1ffdbdaa
Imran Ahmed <96imranahmed@gmail.com>
2017-09-11 19:15:13 +0100

    Enabled search

--- application.py
+++ application.py
@@ -1,17 +1,22 @@
 from datetime import datetime
 import pandas as pd
-from flask import Flask, abort, flash, redirect, render_template, request, url_for
+import numpy as np
+from flask import Flask, abort, flash, redirect, render_template, request, url_for, jsonify
 
+import pickle
+from search import search
 
 app = Flask(__name__)
+search_arr = pickle.load(file = open('./search_array.pickle', 'rb'))
 
 @app.route('/')
 def main():
-    return render_template('index.html', title='Landing!')
+    return render_template('index.html')
 
-@app.route('/<name>')
-def hello_name(name):
-    return "Hello {}!".format(name)
+@app.route('/search/')
+def search_decorator():
+    query = request.args.get('term')
+    return jsonify(search(query, search_arr, 0, 10))
 
 # run the app.
 if __name__ == "__main__":
--- static/js/index.js
+++ static/js/index.js
@@ -1,3 +1,14 @@
 particlesJS.load('particles-js', '/static/js/particle_config.json', function() {
     console.log('callback - particles.js config loaded');
 });
+
+$( document ).ready(function() {
+    $('.ui.search').search({
+        apiSettings: {
+            url: 'search/?term={query}'
+        },
+      type: 'category'
+    });
+});
+
+
